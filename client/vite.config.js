8cb5786d8771e337b3f2444652d923f6186b2eed
A Stone <simplererstone@gmail.com>
2023-11-08 22:26:31 -0500

    m

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,11 +11,11 @@ import react from '@vitejs/plugin-react'
 // Here I am establishing my proxy which allows fetch calls between my server and my client
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3001'
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': 'http://localhost:3001'
+  //   }
+  // },
   plugins: [react()],
 })
 
@@ -24,7 +24,7 @@ export default defineConfig({
 
 // run build when ready to deploy. Should have dist folder ready. Take dist folder and copy whole folder into server folder.
 
-app.use(express.static('dist')); 
+// app.use(express.static('dist')); 
 // change to app.use(express.static('dist'))
 
 // when the frontend is ready I can run only local host and get the application running wiht just the dist. If any changes need to be made however I will need to delete dist folder, make changes, and then rebuild 
